d4fd23d3bdd6a76fc71fd7ee69eff94a342b1526
Marina Garipova <naumarina@yandex.ru>
2023-10-10 21:01:04 +0800

    Step second: modify gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1> <filepath2>')
+
 program.parse();
