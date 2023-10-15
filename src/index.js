66f0e376fca7a945b49c8aeebf2cdfd75913bc40
Marina Garipova <naumarina@yandex.ru>
2023-10-15 22:23:47 +0800

    step fourth

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import _ from 'lodash';
 import { parse } from './parse.js';
 import fs from 'fs';
 import path from 'path';
@@ -10,22 +10,42 @@ const getExtName = (file) => path.extname(file);
 
 const readData = (file) => fs.readFileSync(file, 'utf-8');
 
+const getDiffren = (data1, data2) => {
+    const objKey1 = Object.keys(data1);
+    const objKey2 = Object.keys(data2);
+    const unionKeys = _.sortBy(_.union(objKey1, objKey2));
+    const keys = unionKeys.map((key) => {
+        if (!_.has(data1, key)) {
+            return `  + ${key}: ${data2[key]}\n`;
+        } if (!_.has(data2, key)) {
+            return `  - ${key}: ${data1[key]}\n`;
+        }
+          if (_.has(data1, key) && _.has(data2, key) && data1[key] === data2[key]) {
+            return `    ${key}: ${data1[key]}\n`;
+      }
+      if (_.has(data1, key) && _.has(data2, key) && data1[key] !== data2[key]) {
+        return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}\n`;
+        }
+    });
+    return `{\n${keys.join('')}}`;
+};
+
 const gendiff = (file1, file2) => {
     const absolutePath1 = getAbsolutePath(file1);
     const absolutePath2 = getAbsolutePath(file2);
-        // console.log(absolutePath2);
+
     const data1 = readData(absolutePath1, 'utf-8');
     const data2 = readData(absolutePath2, 'utf-8');
-    // console.log(data2);
+ 
     const extname1 = getExtName(file1);
     const extname2 = getExtName(file2);
-    // console.log(extname2);
+
     const data1Parse = parse(data1, extname1);
     const data2Parse = parse(data2, extname2);
-    // console.log(data2Parse, data1Parse);
-    return { data1Parse, data2Parse };
+  
+    const diffren = getDiffren(data1Parse, data2Parse);
+
+    return diffren;
 };
-// gendiff('file1.json', 'file2.json')
-// console.log(gendiff('file1.json', 'file2.json'));
 
 export default gendiff;
