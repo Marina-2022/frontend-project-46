6027de0e429429101eadc9f9e533439f2d3d67cf
Marina Garipova <naumarina@yandex.ru>
2023-10-17 19:11:26 +0800

    add rul in  eslintrc.yml and fix index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,19 @@
-import fs from 'fs';
+import path from 'path';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
 import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 
-test('genDiff', () => {
-  const expected = fs.readFileSync('./__fixtures__/expected.txt', 'utf-8');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-  const file1 = './__fixtures__/file1.json';
-  const file2 = './__fixtures__/file2.json';
+const expectedPath = getFixturePath('expected.txt');
+
+test('genDiff', () => {
+  const expected = readFileSync(expectedPath, 'utf-8');
+
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
   expect(genDiff(file1, file2)).toEqual(expected);
 });
